6e1a5881618b17696cd5d9b879b0e3e1e5243043
TechBliz <82277875+TechBliz@users.noreply.github.com>
2021-04-17 23:11:43 -0300

    folders

---
+++ assets/theme/js/bgMusic.js
@@ -0,0 +1,21 @@
+var x = document.getElementById("bg_music");
+
+function showVideo() {
+    if (document.getElementById('modalWindow').style.display == 'none') {
+        document.getElementById('modalWindow').style.display = 'block';
+    }
+}
+
+function enableAutoPlay() {
+    x.volume = 0.01;
+    x.autoplay = true;
+    x.load();
+}
+
+function enableMute() {
+    x.muted = true;
+}
+
+function disableMute() {
+    x.muted = false;
+}
---
+++ assets/theme/js/section.js
@@ -0,0 +1,23 @@
+function body() {
+    document.getElementById('gamebody').style.overflow = "auto";
+}
+
+function sections() {
+    document.getElementById('gameplay').style.display = 'block';
+    document.getElementById('forLoad').style.display = 'none';
+    document.getElementById('nav').style.display = 'none';
+}
+
+function showGameplay() {
+    if (document.getElementById('gameplay').style.display = 'none') {
+        document.getElementById('gameplay').style.display = 'block'
+        document.getElementById('nav').style.display = 'none';
+    }
+}
+
+function showNav() {
+    if (document.getElementById('nav').style.display = 'none') {
+        document.getElementById('nav').style.display = 'block'
+        document.getElementById('gameplay').style.display = 'none';
+    }
+}
